00adba7fb01a1b8140307901b530211fac523785
jadorks <adorkorj@gmail.com>
2023-04-05 16:11:41 +0000

    Add logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <img src="/flux_logo.svg" alt="logo" />
+        </div>
+        <div className={styles.nav}>
+          <ul className={styles.nav__menu}>
+            <li>Home</li>
+            <li>Dashboard</li>
+            <li>Referrals</li>
+            <li>About Us</li>
+            <li>
+              <button>Trade Now</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
